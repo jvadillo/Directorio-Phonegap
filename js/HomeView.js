5d11d502d9c249e92fd09d4612ecfa895cf1198f
Jon Vadillo <vadillo.jon@gmail.com>
2013-06-08 15:59:18 -0400

    efecto click

--- js/HomeView.js
+++ js/HomeView.js
@@ -11,7 +11,8 @@ var HomeView = function(store) {
 	    this.el.html(HomeView.template());
 	    return this;
 	};
- 
+
+	
     this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
         this.el = $('<div/>');
